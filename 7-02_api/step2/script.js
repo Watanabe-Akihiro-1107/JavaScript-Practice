6c0cc4946303dca061b3cf8740338e62c79df7d6
渡辺誠大 <answer.is.near.2@gmail.com>
2021-03-20 23:53:08 +0900

    (JavaSprict 超入門 step7-02/step2/index)

---
+++ 7-02_api/step2/script.js
@@ -0,0 +1,34 @@
+'use strict';
+// geolocation
+function success(pos){
+	ajaxRequest(pos.coords.latitude, pos.coords.longitude);
+}
+
+function fail(error){
+	alert('位置情報の取得に失敗しました。エラーコード' + error.code);
+}
+
+navigator.geolocation.getCurrentPosition(success,fail);
+
+//データ取得
+function ajaxRequest(lat,long){
+	const url = 'https://api.openweathermap.org/data/2.5/forecast';
+	const appId = '413a4e16a5e91f7b1349b5ad9583a7b2';
+
+	$.ajax({
+		url: url,
+		data:{
+			appid: appId,
+			lat: lat,
+			lon: long,
+			units: 'metric',
+			lang: 'ja'
+		}
+	})
+	.done(function(data){
+		console.log(data);
+	})
+	.fail(function(){
+		console.log('$.ajax failed!')
+	})
+}
